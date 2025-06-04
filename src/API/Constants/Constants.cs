d7060c13a401fe5ccc7c552e2a849adef54e8347
Mohammad Mahdi Moghaddam <mahdimoghaddamwork@gmail.com>
2025-06-04 20:58:47 +0330

    Add Swagger enhancements and refactor API setup
    Introduced Swagger UI with custom configurations for better API documentation. Replaced weather forecast endpoint with streamlined setup and added a constants class for reusable values. Updated launch URL to point to Swagger and included new dependencies for versioning and Swagger support.

---
+++ src/API/Constants/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace CFour.Constants;
+
+/// <summary>
+/// A static class containing constant values used throughout the project.
+/// </summary>
+public static class Constants
+{
+    public const string ApiDocumentationTitle = "CFour - Back-End APIs";
+}
---
+++ src/API/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace CFour.Extensions;
+
+/// <summary>
+/// IApplicationBuilder extensions for Swagger middlewares setup
+/// </summary>
+internal static class ApplicationExtensions
+{
+    /// <summary>
+    /// application middleware for swagger
+    /// </summary>
+    /// <param name="app"></param>
+    public static void UseCustomSwagger(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.DocumentTitle = Constants.Constants.ApiDocumentationTitle;
+            options.DocExpansion(DocExpansion.None);
+            options.DisplayRequestDuration();
+            options.EnableDeepLinking();
+            options.EnableFilter();
+            options.ShowExtensions();
+            options.DefaultModelRendering(ModelRendering.Model);
+            options.ShowCommonExtensions();
+            options.EnableValidator();
+        });
+    }
+}
